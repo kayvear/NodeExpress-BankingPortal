35e7e3090a4fe8ab2ccbe090069739413832fc1d
Krish <krish@MacPro.local>
2020-12-30 10:47:03 -0500

    Fixed issues

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ const express = require('express');
 
 const app = express();
 
-app.set('views',path.join(__dirname));
+app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('index', { title: 'Index'}));
+
+app.listen(3000, () => { console.log('Express server running on port 3000')});
 
